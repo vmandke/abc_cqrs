c9e30263ae82c11f9adca1cd4f04933e977b70d7
Vinaya Mandke <vmandke90@gmail.com>
2018-09-01 22:09:28 +0530

    Minimal Setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='parking_lot',
+      version='0.1.0',
+      description='Parking Lot: Go Jek Interview',
+      url='',
+      license='Proprietary',
+      author='Vinaya Mandke',
+      author_email='vmandke90@gmail.com',
+      packages=find_packages(),
+      package_data={'parking_lot': []},
+      include_package_data=True,
+      install_requires=[])
